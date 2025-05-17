10961e85285be725c1ee06afb2d8676f5c26640d
Saymon Felix <saymon.felix@aluno.imepac.edu.br>
2025-05-17 13:38:27 -0300

    Estrutura inicial

---
+++ clinica-medica-administrativo/src/main/java/br/edu/imepac/administrativo/ClinicaMedicaAdministrativoApp.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaMedicaAdministrativoApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicaMedicaAdministrativoApp.class, args);
+    }
+}
